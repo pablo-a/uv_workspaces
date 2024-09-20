591fd66355db38e8bc61bd35b6ee34a70993c4e9
Pablo Abril <pablo.abril@deepki.com>
2024-09-20 11:46:29 +0200

    init project

---
+++ src/uv_workspaces/hello.py
@@ -0,0 +1,11 @@
+from p1.hello import main as main_p1
+# from packages.p1.hello import main as main_p1
+
+
+def main():
+    main_p1()
+    print("Hello from uv-workspaces!")
+
+
+if __name__ == "__main__":
+    main()
